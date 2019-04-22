40ec9a98945252bec72af9c0ca1c2ca604aa2f69
keru1264 <32866993+keru1264@users.noreply.github.com>
2019-04-22 12:20:41 +1000

    asdf

---
+++ index.php
@@ -0,0 +1,142 @@
+<?php
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+    throw new Exception(sprintf('Please run "composer require google/apiclient:~2.0" in "%s"', __DIR__));
+}
+require_once __DIR__ . '/vendor/autoload.php';
+
+if (empty($_GET['id'])){
+    return require_once 'setup.php';
+}
+extract(GetParam($_GET));
+
+$fonts = [
+    '1' => ['\'Noto Serif\'' => 'url(\'fnt/noto-serif-v7-latin-regular.woff2\') format(\'woff2\')' ],
+    '2' => ['\'Ubuntu Mono\'' => 'url(\'fnt/ubuntu-mono-v8-latin-regular.woff2\') format(\'woff2\')'],
+    '3' => ['\'VT323\'' => 'url(\'fnt/vt323-v10-latin-regular.woff2\') format(\'woff2\')'],
+    '4' => ['\'SHPinscher-Regular\'' => 'url(\'fnt/SHPinscher-Regular.woff2\') format(\'woff2\')'],
+    '5' => ['\'33535gillsansmt\'' => 'url(\'fnt/33535gillsansmt.woff\') format(\'woff\')']
+];
+
+$shadows = sprintf('-0 -%1$s %2$s #000000, 0 -%1$s %2$s #000000, -0 %1$s %2$s #000000, 0 %1$s %2$s #000000, -%1$s -0 %2$s #000000,
+	%1$s -0 %2$s #000000, -%1$s 0 %2$s #000000, %1$s 0 %2$s #000000, -1px -%1$s %2$s #000000, 1px -%1$s %2$s #000000,
+	-1px %1$s %2$s #000000, 1px %1$s %2$s #000000, -%1$s -1px %2$s #000000, %1$s -1px %2$s #000000, -%1$s 1px %2$s #000000,
+	%1$s 1px %2$s #000000, -2px -%1$s %2$s #000000, 2px -%1$s %2$s #000000, -2px %1$s %2$s #000000, 2px  %1$s %2$s #000000,
+	-%1$s -2px %2$s #000000, %1$s -2px %2$s #000000, -%1$s 2px %2$s #000000, %1$s 2px %2$s #000000, -%1$s -%1$s %2$s #000000,
+	%1$s -%1$s %2$s #000000, -%1$s %1$s %2$s #000000, %1$s %1$s %2$s #000000, -%1$s -%1$s %2$s #000000, %1$s -%1$s %2$s #000000,
+	-%1$s %1$s %2$s #000000, %1$s %1$s %2$s #000000', $line, $thicc);
+	
+function GetParam(array $get):array {
+    $ready_style = [
+        'fontsize' => null,
+        'color' => null,
+        'bgcolor' => null,
+        'line' => null,
+        'thicc' => null,
+		'align' => null
+    ];
+    $styles =[
+        'fontsize' => ['chr' => false, 'min' => 1, 'max' => 480, 'def' => '79px'],
+        'color' => ['chr' => true, 'def' => '#ffffff'],
+        'bgcolor' => ['chr' => true, 'def' => '#00FF00'],
+        'line' => ['chr' => false, 'min' => 0, 'max' => 6, 'def' => '6px'],
+        'thicc' => ['chr' => false, 'min' => 0, 'max' => 6, 'def' => '3px'],
+		'align' => ['chr' => true, 'def' => 'auto']
+        ];
+
+    foreach ($styles as $key => $value){
+        if (isset($get[$key])){
+            if ($value['chr']){
+                $ready_style[$key] = $get[$key];
+            }elseif (is_numeric($get[$key]) && $get[$key] >= $value['min'] && $get[$key] <= $value['max']){
+                $ready_style[$key] = $get[$key]."px";
+            }else{
+                $ready_style[$key] = $value['def'];
+            }
+        }else{
+            $ready_style[$key] = $value['def'];
+        }
+    }
+    return $ready_style;
+}
+
+foreach ($fonts as $key => $value){
+    if ($_GET['font'] == $key){
+       $font = key($value);
+       $fonturl = $value[$font];
+       break;
+    } else {
+        $font = '\'Roboto\'';
+        $fonturl = 'url(\'fnt/Roboto.woff2\') format(\'woff2\')';
+    }
+}
+
+$client = new Google_Client();
+$client->setApplicationName('kaloviy counter');
+$client->setDeveloperKey('API_KEY');
+
+$service = new Google_Service_YouTube($client);
+
+try {
+	$response = $service->videos->listVideos('liveStreamingDetails', ['id' => $_GET['id']]);
+	if(!$response['items']) {
+		$stat = '';
+	} else {
+		$stat = $response['items'][0]['liveStreamingDetails']['concurrentViewers'];
+	}
+}
+catch(Exception $e) {
+	$color = 'red';
+	$bgcolor = 'black';
+	$fontsize = '20px';
+	$thicc = '1px';
+	$line = '0px';
+	$stat = 'daily quota was hit, try later';
+}
+?>
+<html>
+<title>kaloviy counter</title>
+<head>
+
+<script src="js/jquery-3.4.0.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function re() {
+	$.ajax({url: location.href,
+		success: function(data) {
+			$("#re").html($(data).find('#stat'));
+		}, 
+		complete: function() {
+			setTimeout(re, 30000);
+		}
+	})
+})
+</script>
+<style>
+@font-face {
+	font-family: <?php echo $font; ?>;
+	src: <?php echo $fonturl; ?>;
+}
+html {
+	text-align: <?php echo $align; ?>;
+	background: <?php echo $bgcolor; ?>;
+	font-family: <?php echo $font; ?>;
+	font-size: <?php echo $fontsize; ?>;
+	text-shadow: <?php echo $shadows; ?>;
+	
+}
+#re p {
+	margin-top: 0px;
+	margin-bottom: 0px;
+	margin-left: 4px;
+	color: <?php echo $color; ?>;"
+</style>
+</head>
+<body>
+<div id="re">
+<p id="stat"><?php echo $stat; ?></p>
+</div>
+<noscript>
+<p style="color: white; font-size: 16px;">turn on javascript to auto-update</p>
+</noscript>
+</body>
+</html>
---
+++ setup.php
@@ -0,0 +1,70 @@
+<html>
+<title>kaloviy counter</title>
+<head>
+<style>
+@font-face {
+	font-family: 'Roboto';
+	src: url('fnt/Roboto.woff2') format('woff2');
+	font-weight: normal;
+	font-style: normal;
+}
+html, form, input, .btn {
+	 text-align: center;
+	 font-family: Roboto;
+	 font-size: 19px;
+}
+form select, form input {
+	margin-top: 8px;
+	margin-bottom: 10px;
+}
+#thicc, #line {
+	width: auto;
+}
+</style>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script type="text/javascript">
+window.onload = function() {
+	thicc.value = 3
+	line.value = 6
+	fontsize.value = 79
+	align.checked = true
+	stream.value = ""
+	font.value = "0"
+	color.value = "#ffffff"
+	bgcolor.value = "#00FF00"
+}
+</script>
+<script src="js/jquery-3.4.0.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<form action="" method="get">
+	<h6>stream id:</h6>
+    <input type="text" name="id" id="stream" minlength="11" maxlength="11" placeholder="e.g hHW1oY26kxQ" required="required">
+	<h6>font:</h6>
+    <select name="font" id="font">
+		<option value="0">Roboto</option>
+		<option value="1">Noto Serif</option>
+		<option value="2">Ubuntu Mono</option>
+		<option value="3">VT323</option>
+		<option value="4">SH Pinscher</option>
+		<option value="5">Gill Sans MT</option>
+	</select>
+	<h6>size:</h6>
+	<input type="number" name="fontsize" id="fontsize" value="79" min="1" max="480">
+	<h6>thicc:</h6>
+	<input type="range" class="custom-range" name="thicc" id="thicc" value="3" min="0" max="6" oninput="thicc_value.value=this.value">
+	<output id="thicc_value">3</output><h6>smooth:</h6>
+	<input type="range" class="custom-range" name="line" id="line" value="6" min="0" max="6" oninput="line_value.value=this.value">
+	<output id="line_value">6</output>
+	<h6>align text (disable for obs):</h6>
+	<input type="checkbox" checked="true" name="align" id="align" value="center">
+	<h6>color:</h6>
+    <input type="color" name="color" id="color" value="#ffffff">
+	<h6>bgcolor:</h6>
+    <input type="color" name="bgcolor" id="bgcolor" value="#00FF00">
+	<p><input type="submit" class="btn btn-primary" value="OK"></p>
+</form>
+</body>
+</html>
